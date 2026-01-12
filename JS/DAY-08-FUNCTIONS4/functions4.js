8694fa804406c2ac7ed77841a80ac4ea428915f7
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-01-12 16:14:19 +0530

    DAY-08 -GENERATOR

--- JS/DAY-08-FUNCTIONS4/functions4.js
+++ JS/DAY-08-FUNCTIONS4/functions4.js
@@ -1,5 +1,5 @@
-//h Callback Functions:
-
+//h 6. Callback Functions:
+{
 //! Example of Callback Functions:
 
 //? 1st way to create a callback function:
@@ -14,7 +14,7 @@ function sir(studentName , callback){
     },3000);
 }
 
-//~ Callback Function:
+//~ Example of Callback Function:
 function result(){
     console.log("Pass✅");
 }
@@ -33,4 +33,63 @@ orderFood(()=>{
 orderFood(function(){
     console.log("Suresh is your delivery boy!");
 });
+}
+//h 7. HOF - Higher-Order Function:
+{
+//& A function which will accept another function as a agrument is called as Higher Order Function. OR .
+//& A function which will return another function is called Higher Order Function.
 
+//?Example of HOF : calculate is a HOF && add is a Callback Function
+function calculate(num1,num2,operation){//* HOF
+    return operation(num1,num2);
+}
+function add(a,b){//* Callback Function
+    return a + b;
+}
+function sub(a,b){//* Callback Function
+    return a - b;
+}
+console.log(calculate(10,20,add));//* 30
+console.log(calculate(10,20,sub));//* -10
+}
+//h 8. Nested Functions:
+{
+//& The function which is present inside another function is known as Nested Function.
+
+function outer(){
+    console.log("This is Outer Function");
+    let outerVar = "Outer Variable";
+    function inner(){
+        console.log("This is Inner Function");
+        console.log(outerVar);
+    }
+    inner();
+}
+
+outer();
+//h 9. CLOSURE FUNCTION:
+//&  If you are trying to access the outer function variable or data inside the inner function that time it will create the closure by the outer function.
+//? Here , closure means remembering the value of outer function inside the inner function.
+}
+//h 10. Generator Function:
+{
+    //& The generator function is a special type of function which will pause and resume its execution.
+    
+    //? Syntax : function* generatorFunction(){.Code Block.}
+    //& yield -> pause the execution and store the value.
+    //& next() -> resume the execution from the last paused point and again pause it.
+
+    function* netflixSeries(){
+        yield "Episode 1";
+        yield "Episode 2";
+        yield "Episode 3";
+        return "All Episodes are completed!";
+    }
+
+    let episode = netflixSeries();
+    console.log(episode.next());
+    console.log(episode.next());
+    console.log(episode.next());
+    console.log(episode.next());
+
+}
