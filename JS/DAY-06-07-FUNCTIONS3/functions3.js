14a141f6f1fe554b2d61a3d4d3d10ec6440489b5
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-03-07 11:44:13 +0530

    Enhance callback function examples and clarify arrow function behavior

--- JS/DAY-06-07-FUNCTIONS3/functions3.js
+++ JS/DAY-06-07-FUNCTIONS3/functions3.js
@@ -13,7 +13,8 @@ getNumbers(10,20,30); // This will give an error
 //     console.log(arguments);
 // }
 // getNums(60,70,80); // This will also give an error
-
+//arrow function doesnot take the arguments.
+//arrow function directly takes the binding the parent like the this function.
 
 //! Error: functions3.js:13 Uncaught ReferenceError: arguments is not defined
 
@@ -52,14 +53,17 @@ demo();
 
 //h CALLBACK FUNCTION: 
 //& A fucntion which is passed to another function as an argument is called as Callback Function.
+//!There are 3 ways for calling a callback function.
 
+//~ 1. Callback function using variable.
+//?main function
 // OrderFood:
 function orderFood(waiter){
     console.log(`🧑🏽‍🍳: Sir what do you want?
         1. Bhakhari with Chai
         2. Chass`);
     //console.log(waiter);
-    waiter();
+    waiter(); //!it is the callback function.
 }
 
 console.log("ORDER FOOD:");
@@ -67,16 +71,30 @@ orderFood(serveFood);
 console.log("REVIEW FOOD:");
 orderFood(reviewFood);
 
+//? callback function
 //serveFood:
 function serveFood(){
     console.log("🧑🏽‍🍳: Enjoy your meal!");
 }
 
+//? callback function
 //reviewFood:
 function reviewFood(){
     console.log("🧑🏽‍🍳: Please give your feedback!");
 }
 
+//~ 2. Callback function by the use of Arrow Function :->
 document.getElementById("orderserve").addEventListener("click", () =>{orderFood(serveFood);});
 document.getElementById("orderreview").addEventListener("click", () =>{orderFood(reviewFood);});
 
+//~ 3. Callback function by the use of Anonymous Function :->
+function assignment(student){
+    console.log("Teacher assigned you one task!");
+    //console.log(student)
+    student();
+}
+
+//? we can pass directly callback function as anonymous function  or arrow function inside the main function as a argument.
+assignment(function (){
+    console.log("Assignment compeleted teacher has given 100 marks.")
+})
